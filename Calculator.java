8de0076c30e063c025d0ce903f6951d7cd6e909c
yashika-panjwani <123976529+yashika-panjwani@users.noreply.github.com>
2023-08-09 22:26:51 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+package calculatorpackage;
+class Cal{
+    public void meth1(){
+        System.out.println("meth1");
+    }
+}
+class ScCal{
+    public void meth2(){
+        System.out.println("meth2");
+    }
+}
+class HybridCal{
+    public void meth3(){
+        System.out.println("meth3");
+    }
+}
+public class Calculator{
+    public static void main(String[] args){
+    System.out.println("problem 1");
+
+    }
+}
